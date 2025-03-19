b1bcadee618470a61d8bffca9c519e2075da5e82
flesher <56498635+flesher-dizainer@users.noreply.github.com>
2025-03-20 01:13:59 +0300

    Регистрируем модель CustomUser в админке

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from users.models import CustomUser
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'user_type')
+    list_filter = ('user_type',)
+    search_fields = ['username']
