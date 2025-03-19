c1516c91fee9c8d1f1dc1357e2f5389fb050aee7
flesher <56498635+flesher-dizainer@users.noreply.github.com>
2025-03-20 01:13:18 +0300

    Создаём представление пользователей. Нужно доделать редирект на главную страницу, её пока нет.

--- users/views.py
+++ users/views.py
@@ -1,3 +1,33 @@
+from django.contrib import messages
+from django.contrib.auth.views import LogoutView
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from .forms import CustomUserCreationForm
+from .models import CustomUser
 
-# Create your views here.
+
+# Создадим представление регистрации пользователя
+class RegisterView(CreateView):
+    model = CustomUser
+    form_class = CustomUserCreationForm
+    template_name = 'users/register.html'
+    success_url = reverse_lazy('login')
+
+    def dispatch(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return render(request, 'нужно написать страницу')
+        return super().dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        # После успешной регистрации перенаправляем пользователя на страницу входа
+        return response
+
+class CustomLogoutView(LogoutView):
+    """Представление для выхода пользователей"""
+    #next_page = reverse_lazy('core:index')
+
+    def post(self, request, *args, **kwargs):
+        messages.success(request, 'Вы успешно вышли из системы!')
+        return super().post(request, *args, **kwargs)
