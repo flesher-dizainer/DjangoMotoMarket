e6bda828519fc8613aa2143dcbaa5d17f5e5180b
flesher <56498635+flesher-dizainer@users.noreply.github.com>
2025-03-21 00:45:21 +0300

    Добавление приложения core. Добавление статики и медиа путей.

--- DjangoMotoMarket/settings.py
+++ DjangoMotoMarket/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'warehouse',
     'cart',
     'widget_tweaks',
+    'core',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -124,8 +125,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
+# Добавьте эти строки в settings.py
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
